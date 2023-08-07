f485de3b4e6e09995e9d44203b8206cdc7652d81
Owocki <owocki@Owockis-MacBook-Pro.local>
2023-08-07 14:32:45 -0600

    update theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>EndGame</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/owocki/endgame',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/owocki/endgame',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Endgame',
   },
 }
 
