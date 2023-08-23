f971f152aa4170f35dd05af19b0ebb5c569cc82b
Owocki <kevin@supermodular.xyz>
2023-08-23 15:51:00 -0600

    updates:

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>EndGame</span>,
+  logo: <span>K2 EndGame</span>,
   project: {
     link: 'https://github.com/endgameinitiative/k2/tree/main',
   },
   docsRepositoryBase: 'https://github.com/endgameinitiative/k2/tree/main',
   footer: {
-    text: 'Endgame',
+    text: 'K2-Endgame',
   },
   useNextSeoProps() {
     return {
